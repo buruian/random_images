e29d64ad78bb0f5f77e6fa585149e1b47d7bcbc6
Valeriu Buruian <valeraburuian@mail.ru>
2019-06-24 14:38:03 +0300

    Updates

--- precache-manifest.d7597e6eac2991ac64ba71d7b44b9d20.js
+++ precache-manifest.d7597e6eac2991ac64ba71d7b44b9d20.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "deb91f89f2e35a1b6a5a377000eeac29",
+    "revision": "ea820f57959029a868ccdff46bd6b354",
     "url": "/random_images/index.html"
   },
   {
-    "revision": "59522a08c6b1d3178e8a",
+    "revision": "79ef5ef70ec5b9b27afe",
     "url": "/random_images/static/css/2.3cf8dc49.chunk.css"
   },
   {
-    "revision": "475fdff2c99c737af260",
+    "revision": "74a1e771e836b3b7f730",
     "url": "/random_images/static/css/main.e8e10f89.chunk.css"
   },
   {
-    "revision": "59522a08c6b1d3178e8a",
-    "url": "/random_images/static/js/2.1a543e43.chunk.js"
+    "revision": "79ef5ef70ec5b9b27afe",
+    "url": "/random_images/static/js/2.abd0d98f.chunk.js"
   },
   {
-    "revision": "475fdff2c99c737af260",
-    "url": "/random_images/static/js/main.57296796.chunk.js"
+    "revision": "74a1e771e836b3b7f730",
+    "url": "/random_images/static/js/main.8ee0ceac.chunk.js"
   },
   {
     "revision": "ad827ab4ac88620b5002",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random_images/precache-manifest.d65dd3f632a8f576753ce0f0fb90e3ac.js"
+  "/random_images/precache-manifest.d7597e6eac2991ac64ba71d7b44b9d20.js"
 );
 
 self.addEventListener('message', (event) => {
