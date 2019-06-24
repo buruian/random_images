d93a21512da88900797a41394ebaf642fe208573
Valeriu Buruian <valeraburuian@mail.ru>
2019-06-24 12:08:42 +0300

    Updates

---
+++ precache-manifest.d65dd3f632a8f576753ce0f0fb90e3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb91f89f2e35a1b6a5a377000eeac29",
+    "url": "/random_images/index.html"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/random_images/static/css/2.3cf8dc49.chunk.css"
+  },
+  {
+    "revision": "475fdff2c99c737af260",
+    "url": "/random_images/static/css/main.e8e10f89.chunk.css"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/random_images/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "475fdff2c99c737af260",
+    "url": "/random_images/static/js/main.57296796.chunk.js"
+  },
+  {
+    "revision": "ad827ab4ac88620b5002",
+    "url": "/random_images/static/js/runtime~main.475d2503.js"
+  }
+]);
